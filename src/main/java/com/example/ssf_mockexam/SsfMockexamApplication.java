aa038ec55cf2d8994cef54def83b76348c8b9d2a
kopiteh <35788631+kopiteh@users.noreply.github.com>
2022-01-11 13:05:27 +0800

    Initial commit

---
+++ src/main/java/com/example/ssf_mockexam/SsfMockexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssf_mockexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfMockexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfMockexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssf_mockexam/SsfMockexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssf_mockexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfMockexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
